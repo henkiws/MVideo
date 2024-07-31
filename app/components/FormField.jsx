35198f263d3b5f17b9105b3bc86de02cab0e0816
henki wisnu subakti <henki.subakti@gmail.com>
2024-07-31 15:31:46 +0700

    create

--- app/components/FormField.jsx
+++ app/components/FormField.jsx
@@ -13,10 +13,10 @@ const FormField = ({ title, value, placeholder, handleChangeText, otherStyles, .
 
             <View className="border-2 border-black-200 w-full h-16 px-4 bg-black-100 rounded-2xl focus:border-secondary items-center flex-row">
                 <TextInput 
-                    className="flex-1 text-white font-psemibold text-base"
+                    className="flex-1 text-white font-pregular text-base"
                     value={value}
                     placeholder={placeholder}
-                    placeholderTextColor="#7b7b8"
+                    placeholderTextColor="gray"
                     onChangeText={handleChangeText}
                     secureTextEntry={title === 'Password' && !showPassword}
                 />
